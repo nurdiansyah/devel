32db899fd47bec8b52a6a7e7700ca3cc46aeba46
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2019-05-08 08:08:24 +0700

    feat: add preact devel module

---
+++ packages/preact/jest/setup.js
@@ -0,0 +1,21 @@
+require('raf/polyfill')
+
+const enzyme = require('enzyme')
+const configure = enzyme.configure
+
+const Adapter = require('enzyme-adapter-react-16')
+configure({adapter: new Adapter()})
+
+global.shallow = enzyme.shallow
+global.render = enzyme.render
+global.mount = enzyme.mount
+global.jsdom = function jsdom(html) {
+  if (typeof document === 'undefined') {
+    const _jsdom = require('jsdom').jsdom
+    global.document = _jsdom(html || '')
+    global.window = global.document.defaultView
+    global.navigator = {
+      userAgent: 'JSDOM'
+    }
+  }
+}
