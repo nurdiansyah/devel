83df7af98fd59316744c72dd1c122651371be8e4
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2020-04-29 12:18:36 +0700

    fix (eslint): rules prettier

--- packages/eslint/rules/prettier.js
+++ packages/eslint/rules/prettier.js
@@ -1,5 +1,8 @@
 const prettierConfig = require('eslint-config-prettier');
 
 module.exports = Object.assign(prettierConfig, {
-  plugins: ['prettier']
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 2
+  }
 });
