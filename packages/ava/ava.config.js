8ff5d1c8ce924652c2cfd7d05f52ef44ce591ed5
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2023-09-05 13:24:11 +0700

    update ava

---
+++ packages/ava/ava.config.js
@@ -0,0 +1,12 @@
+export default {
+  "extensions": {
+    "js": true,
+    "ts": "module"
+  },
+  "nodeArguments": [
+    "--loader=esbuild-node-loader"
+  ],
+  "files": [
+    "src/**/__tests__/**/*.(test|spec).(js|ts)"
+  ]
+}
